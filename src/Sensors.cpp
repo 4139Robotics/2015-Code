9cf181e5a46534fe5b46b7ed3a744a5bb852bcb8
Jeff <jeffreychen1234@gmail.com>
2015-01-30 14:52:03 -0800

    Changes DigitalInput

--- src/Sensors.cpp
+++ src/Sensors.cpp
@@ -1,27 +1,47 @@
-/*
- *  Sensors.cpp
- *  	Sends various data from the sensors.
- *
- *  FRC Team 4139 - Easy as Pi
- *  	Author(s):
- */
-
 #include "WPILib.h"
+#include "gyro.cpp"
+#include "BuiltInAccelerometer.h"
+#include <DigitalInput.h>
 
 struct Sensors_In
 {
-	//
+    
 };
 
 struct Sensors_Out
 {
-	float returnGyroAngle;
-	float returnAccelX, returnAccelY, returnAccelZ;
-	float returnDistance;
+    float returnGyroAngle;
+    float returnAccelX, returnAccelY, returnAccelZ;
+    float returnDistance;
+    bool returnUpperLiftSwitch, returnLowerLiftSwitch;
 };
 
 class Sensors
 {
-
-
-};
+private:
+    GyroSensor* gyro;
+    Accelerometer *accel;
+    DigitalInput *UpperLift;
+    DigitalInput *LowerLift;
+    
+public:
+    Sensors::Sensors() {
+        gyro = new GyroSensor();
+        accel = new BuiltInAccelerometer(Accelerometer:kRange_4G);
+        UpperLift = new DigitalInput(uint32_t 9999);
+        LowerLift = new DigitalInput(uint32_t 9999);
+        
+    }
+    Sensor_Output Run(Sensor_Input input) {
+        Sensors_Output out;
+        
+        out.returnGyroAngle = gyro->GetAngle();
+        out.returnAccelX = accel->GetX();
+        out.returnAccelY = accel->GetY();
+        out.returnAccelZ = accel->GetZ();
+        out.returnUpperLiftSwitch = UpperLift->Get();
+        out.returnLowerLiftSwitch = LOwerLift ->Get();
+    }
+    
+    
+}
