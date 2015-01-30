5fa652824e1128bf89857fd7295e6d685d2cebed
LeDaniel Leung <ledaniel.leung@gmail.com>
2015-01-30 15:08:38 -0800

    Added POV Hat variables

--- src/X360Controller.cpp
+++ src/X360Controller.cpp
@@ -17,7 +17,9 @@ struct X360Controller_In
 struct X360Controller_Out
 {
 	//rotation variables
-	float returnRotation;
+	float returnX,
+		  returnY,
+		  returnRotation;
 	bool returnRotate;
 
 	//left and right analog sticks
@@ -27,6 +29,16 @@ struct X360Controller_Out
 		  rightAnalogY,
 		  leftThrottle,
 		  rightThrottle;
+	//POV hat
+	int center,
+		up,
+	    upRight,
+		right,
+		downRight,
+		down,
+		downLeft,
+		left,
+		upLeft;
 	//buttons
 	bool buttonA,
 	     buttonB,
@@ -60,13 +72,24 @@ public:
 	X360Controller_Out Run(X360Controller_In input)
 	{
 
-		//setting sticks to general values
+		//setting general values to axes
 		Output.leftAnalogX   = stick->GetRawAxis(0);
 		Output.leftAnalogY   = stick->GetRawAxis(1);
 		Output.leftThrottle  = stick->GetRawAxis(2);
 		Output.rightThrottle = stick->GetRawAxis(3);
 		Output.rightAnalogX  = stick->GetRawAxis(4);
 		Output.rightAnalogY  = stick->GetRawAxis(5);
+
+		//setting states to POV values
+		Output.center	 = stick->GetPOV(-1);
+		Output.up 		 = stick->GetPOV(0);
+		Output.upRight   = stick->GetPOV(45);
+		Output.right 	 = stick->GetPOV(90);
+		Output.downRight = stick->GetPOV(135);
+		Output.down 	 = stick->GetPOV(180);
+		Output.downLeft  = stick->GetPOV(225);
+		Output.left 	 = stick->GetPOV(270);
+		Output.upLeft 	 = stick->GetPOV(315);
 
 		//setting buttons to general functions
 		Output.buttonA	    = stick->GetRawButton(0);
@@ -85,18 +108,18 @@ public:
 	//signfinder, used for deadzone
 	float findSign(float num)
 	{
-			if (num > 0)
-				return 1;
-			else if (num < 0)
-				return -1;
-			else
-				return 0;
-		}
+		if (num > 0)
+			return 1;
+		else if (num < 0)
+			return -1;
+		else
+			return 0;
+	}
 
 		//Deadzone due to bad controllers
-		float ApplyDZ(float axis, float deadzone)
-		{
-			return fabs(axis) < deadzone ? 0 : (fabs(axis) - deadzone) / (1	- deadzone) * findSign(axis);
-		}
+	float ApplyDZ(float axis, float deadzone)
+	{
+		return fabs(axis) < deadzone ? 0 : (fabs(axis) - deadzone) / (1	- deadzone) * findSign(axis);
+	}
 
 };
